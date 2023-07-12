8cb08a2e7a2e9318330ea6ad69500ed85bc3f5d0
nildaJarero09 <99058821+nildaJarero09@users.noreply.github.com>
2023-07-12 15:33:00 -0500

    Create bye2.php

---
+++ bye2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ballot 2 Exit</title>
+</head>
+<style>
+    button{
+        background-color: lightgreen;
+        color: black;
+        border: 1px solid darkgreen;
+        font-weight: bold;
+        display: inline-block;
+        padding: 12px 20px;
+        cursor: pointer;
+}
+
+button:hover {
+  background-color: darkgreen;
+}
+
+body{
+    background-color: lightgray;
+    font-weight: bold;
+    text-align: center;
+    font-size: x-large;
+    padding-top: 35%;
+}
+    
+</style> 
+
+<body>
+    
+
+    Thank you for voting. Please notify the research instructor that you are finished.
+    <br><br><br>
+    <a href="index2.php">
+        <button name="vote">Back to Home Screen</button>
+    </a>
+    
+   
+   
+</body>
+</html>
