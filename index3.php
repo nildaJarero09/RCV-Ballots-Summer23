259ab0da358911e852425e57738ff503ba3ff83a
nildaJarero09 <99058821+nildaJarero09@users.noreply.github.com>
2023-07-12 15:04:02 -0500

    Create index3.php

---
+++ index3.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ballot 3 Welcome</title>
+</head>
+<style>
+    button{
+        background-color: lightgreen;
+        color: black;
+        border: 1px solid darkgreen;
+        font-weight: bold;
+        display: inline-block;
+        padding: 12px 20px;
+        cursor: pointer;
+}
+
+button:hover {
+  background-color: darkgreen;
+}
+
+body{
+    background-color: lightgray;
+    font-weight: bold;
+    text-align: center;
+    font-size: x-large;
+    padding-top:20%;
+    
+}
+    
+</style> 
+
+<body>
+    
+
+    Click "Vote" to begin the voting process.
+    <br><br><br>
+    <a href="voting.php">
+        <button type="submit" name="submit">Vote</button>
+    </a>
+
+    
+
+   
+   
+</body>
+</html>
